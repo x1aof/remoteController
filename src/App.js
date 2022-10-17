1b60cbe28eb2d75d19589d3c482a724dec131be1
x1aof <1048184669@qq.com>
2022-10-17 20:14:00 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { Radio } from "antd";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState(1);
+  const onChange = (e) => {
+    setValue(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const [switc, setSwitc] = useState(true);
+  const switcClick = () => {
+    setSwitc((off) => !off);
+    console.log(switc);
+  };
+
+  const remoteUpClick = () => {
+    console.log("up");
+  };
+
+  const remoteDownClick = () => {
+    console.log("down");
+  };
+
+  const remoteLeftClick = () => {
+    console.log("left");
+  };
+
+  const remoteRightClick = () => {
+    console.log("right");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Radio.Group onChange={onChange} value={value}>
+          <Radio value={1}>遥控模式 </Radio>
+          <Radio value={2}>AI模式</Radio>
+        </Radio.Group>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="control-wrapper">
+          <div
+            onClick={remoteUpClick}
+            className={value === 1 ? "control-btn control-top" : ""}
+          ></div>
+          <div
+            onClick={remoteLeftClick}
+            className={value === 1 ? "control-btn control-left" : ""}
+          >
+            {" "}
+          </div>
+          <div
+            onClick={remoteDownClick}
+            className={value === 1 ? "control-btn control-bottom" : ""}
+          ></div>
+          <div
+            onClick={remoteRightClick}
+            className={value === 1 ? "control-btn control-right" : ""}
+          ></div>
+
+          <div className="control-round">
+            <div className="control-round-inner" onClick={switcClick}>
+              {switc === true ? "OFF" : "ON"}
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
